aa9c00ba484db7c967355ab0ec5fa4a1a7f2ddf9
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-22 17:55:53 -0300

    feat: add Movie entity and NowPlayingResponse interface for movie data handling

---
+++ src/core/use-cases/movies/now-playing.use-cases.ts
@@ -0,0 +1,15 @@
+import { HttpAdapter } from '../../../config/adapters/http/http.adapter';
+import { NowPlayingResponse } from '../../../infrastructure/interfaces/movie-db.responses';
+import { Movie } from '../../entities/movie.entity';
+
+export const moviesNowPlayingUseCase = async (
+  fetcher: HttpAdapter,
+): Promise<Movie[]> => {
+  try {
+    const nowPlaying = await fetcher.get<NowPlayingResponse>( '/now_playing' );
+    console.log( nowPlaying );
+    return [];
+  } catch {
+    throw new Error( 'Error fetching movies - NowPlaying' );
+  }
+};
